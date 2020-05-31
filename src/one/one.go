6483b49304a19dcbe1d32b633717f95257bee825
joshlobo4289 <joshua.jude.lobo@gmail.com>
2020-05-31 17:55:04 +0100

    divide example function

--- src/one/one.go
+++ src/one/one.go
@@ -1,8 +1,19 @@
 package one 
-import (
-	"fmt"
-)
-func One(){
-	fmt.Println("Welcome to Package One ")
+ import (
+ 	"fmt"
+ 	"errors"
+ )
+
+ func One(){
+ 	fmt.Println("Welcome to Package One")
 	
+}                                                                                                                                                                           
+
+func Divide(p1,p2 float64)(ans float64 ,err error){
+ 	if p2 == 0 {
+ 		err = errors.New("Cannot divide by 0 ")	
+ 	}
+ 	ans = p1/p2
+ 	return
+// 	// this will return ans 
 }
